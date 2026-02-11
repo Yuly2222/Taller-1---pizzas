631f00070ecc2dd1e6c0da6a5cc9dd58ece649fc
Yuly :) <159471511+Yuly2222@users.noreply.github.com>
2026-02-11 17:25:12 -0500

    Clases tipo de pizzas

---
+++ src/main/java/edu/unisabana/pizzafactory/model/HorneadorPizzaIntegral.java
@@ -0,0 +1,20 @@
+
+package edu.unisabana.pizzafactory.model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author cesarvefe
+ */
+public class HorneadorPizzaIntegral {
+
+    public void hornear() {
+        Logger.getLogger(HorneadorPizzaIntegral.class.getName())
+            .log(Level.INFO, "[~~] Horneando la pizza integral con masa convencional.");
+
+        //CODIGO DE LLAMADO AL MICROCONTROLADOR
+    }
+    
+}
---
+++ src/main/java/edu/unisabana/pizzafactory/model/MoldeadorPizzaGruesa.java
@@ -0,0 +1,28 @@
+
+package edu.unisabana.pizzafactory.model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author cesarvefe
+ */
+public class MoldeadorPizzaGruesa {
+
+    public void moldearPizzaPequena() {
+        Logger.getLogger(MoldeadorPizzaGruesa.class.getName())
+                .log(Level.INFO, "[O] Moldeando pizza pequena de masa convencional.");
+        
+        //CODIGO DE LLAMADO AL MICROCONTROLADOR
+
+    }
+
+    public void molderarPizzaMediana() {
+        Logger.getLogger(MoldeadorPizzaGruesa.class.getName())
+                .log(Level.INFO, "[O] Moldeando pizza mediana de masa convencional.");
+        
+        //CODIGO DE LLAMADO AL MICROCONTROLADOR
+    }
+
+}
